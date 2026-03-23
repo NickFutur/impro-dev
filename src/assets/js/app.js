89493b7c88ea262ae846f1627aa82fed512b836e
DanilMorin <DanilMorin>
2026-03-23 18:02:22 +0300

    Responsive Display animation added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -166,3 +166,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
   observer.observe(header);
 });
+
+//слайдер с страницы шаблоны "
+$(document).ready(function(){
+
+  $('.blog__list').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+
+    prevArrow: '.blog__left',
+    nextArrow: '.blog__right',
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+  });
+
+});
